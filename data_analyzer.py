614e999d1f5a2bbb818e58a385293f21e10fafba
PMarr62 <117673241+PMarr62@users.noreply.github.com>
2025-07-24 15:06:18 -0500

    Updated data_analyzer with method headers

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,8 +1,27 @@
 from apireader import APIReader
+import json
 
 class DataAnalyzer:
     def __init__(self, coins=0):
-        self.api_reader = APIReader()
-        self.coins = coins
+        self.api_reader: APIReader = APIReader()
+        self.coins: int = coins
+        self.recipes: dict = {}
+
+    def read_recipe_json(self, recipe_path):
+        with open(recipe_path, 'r') as f:
+            self.recipes = json.load(f)
+
+    def buy_order(self):
+        pass
+
+    def sell_offer(self):
+        pass
+
+    def print_results(self):
+        pass
+
+    
+
+    
 
     
